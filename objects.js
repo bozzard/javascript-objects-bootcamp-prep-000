6336548708c1610609a86dbcb442aa3f7c1f3482
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-04 10:38:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,13 @@
 var playlist = {
-  artistName: ["john bon jovi", "sandra nasic"],
-  songTitle: ["armageddon", "path"]
+//  artistName: ["john bon jovi", "sandra nasic"],
+//  songTitle: ["armageddon", "path"]
+john bon jovi : armageddon,
+sandra nasic: path
 };
+
+
+/*
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+}
+*/
