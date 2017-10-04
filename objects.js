54343a55dbb69f9df5f6fad309d0453e779ed453
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-04 10:52:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,8 +5,10 @@ john_bon_jovi : "armageddon",
 sandra_nasic: "path"
 };
 
-
-
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist [artistName] = songTitle;
+  return playlist;
+}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist [artistName] = songTitle;
   return playlist;
