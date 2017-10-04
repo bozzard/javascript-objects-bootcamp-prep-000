f219d12cdfccb26feae4ee48faab29ab7ebc3717
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-04 12:16:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,6 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
+  console.log(playlist);
 
   delete playlist[artistName];
   playlist;
